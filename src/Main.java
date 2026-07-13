55d638ac4a60fd459f6116ded3fd2763479c4f61
albe <kikmine090511@gmail.com>
2026-07-14 01:27:39 +0200

    FINITOOIDHAIUHDAIUDIGWAGDUIYAGUIYDAGWGYUDAGY

--- src/Main.java
+++ src/Main.java
@@ -2,83 +2,89 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public Main() {
-    }
-
+    static String tipo;
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.print("Valore placeholder ->");
+        System.out.println("""
+                 _                _           _                         _                   _            \s
+                | |              | |         | |                       | |                 | |           \s
+                | | ___  __ _  __| | ___ _ __| |__   ___   __ _ _ __ __| |  _ __ ___   __ _| | _____ _ __\s
+                | |/ _ \\/ _` |/ _` |/ _ \\ '__| '_ \\ / _ \\ / _` | '__/ _` | | '_ ` _ \\ / _` | |/ / _ \\ '__|
+                | |  __/ (_| | (_| |  __/ |  | |_) | (_) | (_| | | | (_| | | | | | | | (_| |   <  __/ |  \s
+                |_|\\___|\\__,_|\\__,_|\\___|_|  |_.__/ \\___/ \\__,_|_|  \\__,_| |_| |_| |_|\\__,_|_|\\_\\___|_|  \s
+                
+                
+                1. FancyHolograms
+                2. DecentHolograms
+                Selezionare [1,2] ->
+                """);
+        tipo = s.nextLine();
+        System.out.print("\nValore placeholder ->");
         String placeholder = s.nextLine();
         System.out.print("Colore ->");
         String colore = s.nextLine();
         System.out.print("\nTitolo leaderboard ->");
         String titolo = s.nextLine();
         placeholder.replaceAll("%", "");
-        System.out.println("\nColori per top 3? [Y,n]");
+        System.out.println("Colori per top 3? [Y,n]");
         String x = s.nextLine();
         boolean top3colori = yn(x);
         leaderboard(titolo, placeholder, colore, top3colori);
     }
 
     public static void leaderboard(String titolo, String placeholder, String colorep, boolean colori) {
-        if (tipo == 1) {
+        if (tipo.contains("1")) {
             System.out.println("    - '" + titolo + "'");
             String prefix = "";
             for (int posizione = 1; 10 >= posizione; posizione++) {
                 // sparatemi
-                String linea = "    - '" + prefix + posizione + " &f- %ajlb_lb_" + placeholder + "_" + posizione + "_alltime_name% " + colorep + "%ajlb_lb_" + placeholder + "_" + posizione + "_alltime_value%'";
                 if (colori) {
                     if (posizione == 1) {
-                        System.out.println("posizione:" + posizione);
+                        // molto debuggings
+//                        System.out.println("posizione:" + posizione);
                         prefix = "&#FCD05C#";
-                        System.out.println(linea);
                     } else if (posizione == 2) {
                         prefix = "&#BCB8AF#";
-                        System.out.println(linea);
                     } else if (posizione == 3) {
                         prefix = "&#AC8A30#";
-                        System.out.println(linea);
                     } else {
                         prefix = "&7#";
-                        System.out.println(linea);
                     }
-                } else {
-                    System.out.println(linea);
                 }
+                String linea = "    - '" + prefix + posizione
+                        + " &f- %ajlb_lb_" + placeholder + "_" + posizione + "_alltime_name% "
+                        + colorep + "%ajlb_lb_" + placeholder + "_" + posizione + "_alltime_value%'";
+                System.out.println(linea);
 
             }
             System.out.println("    - '" + prefix + "%ajlb_position_" + placeholder + "_alltime% &f- %player_name% "+colorep+"%ajlb_value_" + placeholder + "_alltime%");
 
 
 
-        } else if (tipo == 2) {
+        } else if (tipo.contains("2")) {
             System.out.println("  - content: '"+titolo+"'\n    height: 0.3");
             String prefix = "";
             for (int posizione = 1; 10 >= posizione; posizione++) {
                 // woah ora si vede qualcosa
-                String linea = "  - content: " + prefix
-                        + posizione
-                        + " &f- %ajlb_lb_" + placeholder + "_" + posizione + "_alltime_name% "
-                        + colorep + "%ajlb_lb_" + placeholder + "_" + posizione + "_alltime_value%'";
                 if (colori) {
                     if (posizione == 1) {
-                        System.out.println("posizione:" + posizione);
                         prefix = "&#FCD05C#";
-                        System.out.println(linea);
                     } else if (posizione == 2) {
                         prefix = "&#BCB8AF#";
-                        System.out.println(linea);
                     } else if (posizione == 3) {
                         prefix = "&#AC8A30#";
-                        System.out.println(linea);
                     } else {
                         prefix = "&7#";
-                        System.out.println(linea);
                     }
-                } else {
-                    System.out.println(linea);
                 }
+                String linea = "  - content: '" + prefix
+                        + posizione
+                        + " &f- %ajlb_lb_" + placeholder + "_" + posizione + "_alltime_name% "
+                        + colorep + "%ajlb_lb_" + placeholder + "_" + posizione + "_alltime_value%'\n    height: 0.3";
+                System.out.println(linea);
             }
+            System.out.println("  - content: '&f'\n    height: 0.3");
+            System.out.println("  - content: '"+ prefix + "%ajlb_position_" + placeholder + "_alltime% &f- %player_name% "+colorep+"%ajlb_value_" + placeholder + "_alltime%'"+"\n    height: 0.3");
         }
     }
 
